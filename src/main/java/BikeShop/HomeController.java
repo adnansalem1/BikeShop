84276fbfc834db9a8b0360867f48accf29db32da
Adnan Salem <adnan.salem@outlook.de>
2024-05-28 18:05:21 +0200

    CrossOrigin update

--- src/main/java/BikeShop/HomeController.java
+++ src/main/java/BikeShop/HomeController.java
@@ -13,7 +13,7 @@ public class HomeController {
     @GetMapping("/anzeigen")
     public List<Anzeige> anzeigen() {
         Anzeige anzeige1 = new Anzeige("Test 1", "Test", 2.50);
-        Anzeige anzeige2 = new Anzeige("Test 2 ", "Test", 2.50);
+        Anzeige anzeige2 = new Anzeige("Test 2", "Test", 2.50);
         Anzeige anzeige3 = new Anzeige("Test 3", "Test", 2.50);
         Anzeige anzeige4 = new Anzeige("Test 4", "Test", 2.50);
         return List.of(anzeige1, anzeige2, anzeige3, anzeige4);
