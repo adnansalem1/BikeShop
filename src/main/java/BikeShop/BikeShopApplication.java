c8086ba829dd174489bbc13e91c9059c7a261062
Adnan Salem <adnan.salem@outlook.de>
2024-06-05 22:59:52 +0200

    gradle build update 3

--- src/main/java/BikeShop/BikeShopApplication.java
+++ src/main/java/BikeShop/BikeShopApplication.java
@@ -2,7 +2,6 @@ package BikeShop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
