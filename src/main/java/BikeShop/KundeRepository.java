6c851f06855320e81abae1b51d9fcb9c84e41382
Adnan Salem <adnan.salem@outlook.de>
2024-06-21 18:09:24 +0200

    new class deleted

--- src/main/java/BikeShop/KundeRepository.java
+++
@@ -1,8 +0,0 @@
-package BikeShop;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface KundeRepository extends CrudRepository<Kunde, Long> {
-}
